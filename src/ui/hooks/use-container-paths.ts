a8eb81a24707356e9c7f6c002ad3d6031e565f76
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-08-11 15:55:32 +0300

    feat: handle errors in useContainerPath

--- src/ui/hooks/use-container-paths.ts
+++ src/ui/hooks/use-container-paths.ts
@@ -14,22 +14,53 @@
  * limitations under the License.
  */
 import { useEffect, useState } from "react";
+import { sendNotificationEvent } from "@drill4j/send-notification-event";
 
-const containerPaths = {
+const devModePaths = {
   test2code: "http://localhost:8080/Drill4J-test-to-code.js",
   stateWatcher: "http://localhost:8090/Drill4J-state-watcher-ui.js",
 };
+
+function isValidHttpUrl(string: string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+const validate = (obj: Record<string, string>): boolean => !!Object.entries(obj)
+  .filter(([key, value]) => typeof key === "string" && isValidHttpUrl(value));
 
 export const useContainerPaths = () => {
   const [paths, setPaths] = useState<Record<string, string> | null>(null);
 
   const getContainerPaths = async () => {
     if (process.env.NODE_ENV === "production") {
-      const response = await fetch("/container-paths.json");
-      const data = await response.json();
-      setPaths(data);
+      try {
+        const response = await fetch("/container-paths.json");
+        try {
+          const data = await response.json();
+          if (validate(data)) {
+            setPaths(data);
+          } else {
+            sendNotificationEvent({
+              type: "ERROR",
+              text: "CRITICAL ERROR: unable to obtain plugin resources. This is likely happened due to invalid PLUGINS env variable value",
+            });
+          }
+        } catch {
+          sendNotificationEvent({ type: "ERROR", text: "Failed to parse json" });
+        }
+      } catch (error) {
+        sendNotificationEvent({ type: "ERROR", text: error || "Failed to fetch containers paths" });
+      }
     } else {
-      setPaths(containerPaths);
+      setPaths(devModePaths);
     }
   };
 
