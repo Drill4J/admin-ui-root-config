dbfcb835900783cddefa99c44322e412665651dc
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-02-24 19:03:12 +0300

    fix: add dot in alert messages

--- src/ui/pages/service-group/plugin/plugin.tsx
+++ src/ui/pages/service-group/plugin/plugin.tsx
@@ -34,7 +34,7 @@ export const Plugin = () => {
     const isPluginAlreadyRegistered = getAppNames().includes(getPluginName(pluginId));
     if (isPluginAlreadyRegistered) return;
     if (!paths[pluginId]) {
-      sendAlertEvent({ type: "ERROR", title: "CRITICAL ERROR: Plugin URL is not exist. Check PLUGINS env variable value" });
+      sendAlertEvent({ type: "ERROR", title: "CRITICAL ERROR: Plugin URL is not exist. Check PLUGINS env variable value." });
       return;
     }
     registerAgentPlugin(pluginId, paths[pluginId], {
