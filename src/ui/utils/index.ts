1563501eacab3846fa047f6622450b529c938e45
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-03-01 17:41:00 +0300

    fix: validate used agent and group name

--- src/ui/utils/index.ts
+++ src/ui/utils/index.ts
@@ -14,3 +14,5 @@
  * limitations under the License.
  */
 export { convertAgentName } from "./covert-agent-name";
+export { unusedAgentName } from "./unusedAgentName";
+export { unusedGroupName } from "./unusedGroupName";
---
+++ src/ui/utils/unusedGroupName.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2020 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { FormValidator, getPropertyByPath } from "@drill4j/ui-kit";
+import { Agent } from "../types";
+
+export function unusedGroupName(field: string, agents: { single: Agent[], grouped: any[] }, ignoredName: string): FormValidator {
+  return (valitationItem) => {
+    const value = getPropertyByPath<string>(valitationItem, field);
+    const registeredGroup = agents.grouped.filter(agent => agent.group.name !== ignoredName).map(agent => agent.group.name);
+    const isSomeGroup = registeredGroup.some(agent => agent === value);
+    return isSomeGroup ? { [field]: "Service Group with such name already exists. Please choose a different name." } : {};
+  };
+}
