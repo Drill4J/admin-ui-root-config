a08025b5b8abce0668ca80fbce88224b7ab361a5
dmitrii-kliushentsev <94057436+dmitrii-kliushentsev@users.noreply.github.com>
2022-03-14 04:53:32 +0300

    feat: implement status for packages field in system settings form (#121)

--- src/ui/components/panels/settings/agent-settings/system-settings-form/system-settings-form.tsx
+++ src/ui/components/panels/settings/agent-settings/system-settings-form/system-settings-form.tsx
@@ -13,50 +13,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { useState } from "react";
+import React from "react";
 import {
-  ContentAlert, DarkFormGroup, dotsAndSlashesToSlash, Field, Fields, Icons, Tooltip,
+  DarkFormGroup, dotsAndSlashesToSlash, Field, Fields, Icons, useFormikContext,
 } from "@drill4j/ui-kit";
 
 import "twin.macro";
 
 export const SystemSettingsForm = () => {
-  const [unlockedPackages, setUnlockedPackages] = useState(false);
+  const { status, setStatus } = useFormikContext();
 
   return (
-    <div tw="w-[400px] flex flex-col gap-y-6">
-      {unlockedPackages && (
-        <ContentAlert type="WARNING">
+    <>
+      {status.unlockedPackages && (
+        <div tw="flex w-[400px] gap-x-4 p-6 border border-orange-default rounded-lg text-orange-default text-14 leading-24">
+          <div tw="pt-1">
+            <Icons.Danger />
+          </div>
           Please be aware that any change to the package
-          list will result in a complete loss of gathered
-          data in plugins that have been using these packages.
-        </ContentAlert>
+          list will result in a complete loss of gathered data
+          in plugins that have been using these packages.
+        </div>
       )}
       <DarkFormGroup label={(
         <div tw="flex justify-between w-[400px]">
-          <div tw="flex gap-x-2 items-center">
-            Application Packages
-            <Tooltip
-              message={(
-                <div tw="space-y-2 text-[13px] leading-20">
-                  <div>
-                    Specify all necessary parts of your application.{"\n"}
-                    Make sure you add application packages only,{"\n"}
-                    otherwise Agent&apos;s performance will be affected.
-                  </div>
-                  <div>
-                    Please, use:{"\n"}- new line as a separator;{"\n"}-
-                    &quot;!&quot; before package/class for excluding;{"\n"}-
-                    &quot;/&quot; in a package path.
-                  </div>
-                </div>
-              )}
+          Application Packages
+          {!status.unlockedPackages && (
+            <div
+              onClick={() => setStatus({
+                unlockedPackages: true,
+              })}
+              tw="flex items-center gap-x-2 font-regular cursor-pointer"
             >
-              <Icons.Info />
-            </Tooltip>
-          </div>
-          {!unlockedPackages && (
-            <div onClick={() => setUnlockedPackages(true)} tw="flex items-center gap-x-2 font-regular cursor-pointer">
               <Icons.Lock width={12} height={14} />
               Unlock
             </div>
@@ -68,33 +56,17 @@ export const SystemSettingsForm = () => {
           component={Fields.DarkTextarea}
           name="systemSettings.packages"
           placeholder="e.g. com/example/mypackage&#10;foo/bar/baz&#10;and so on."
-          disabled={!unlockedPackages}
+          disabled={!status.unlockedPackages}
           normalize={(str: string) => dotsAndSlashesToSlash(str).replace(/(?:(?:\r\n|\r|\n)\s*){2}/gm, "")}
         />
       </DarkFormGroup>
-      <DarkFormGroup
-        optional
-        label={(
-          <div tw="flex gap-x-2 items-center h-4">
-            Header Mapping
-            <Tooltip
-              message={(
-                <div tw="space-y-2 text-[13px] leading-20">
-                  Session header name to track User actions on your target app.
-                </div>
-              )}
-            >
-              <Icons.Info />
-            </Tooltip>
-          </div>
-        )}
-      >
+      <DarkFormGroup label="Header Mapping" optional>
         <Field
           name="systemSettings.sessionIdHeaderName"
           component={Fields.DarkInput}
           placeholder="Enter session header name"
         />
       </DarkFormGroup>
-    </div>
+    </>
   );
 };
